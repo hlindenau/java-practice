ba267c818c71792e0bafe33ee815e75dd054940d
Hubert <hubert.lindenau@gmail.com>
2020-10-05 21:13:11 +0200

    github commit test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,25 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Test");
-	System.out.println("Update");
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Piotr");
+        Test t = new Test("Me",0);
+        Test anonim = new Test(100);
+        t.raiseSalary(10);
+
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
+        NumberFormat percentFormatter = NumberFormat.getPercentInstance();
+        double x = 0.1;
+        System.out.println(currencyFormatter.format(x));
+        System.out.println(percentFormatter.format(x));
 
     }
 }
--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -1,5 +1,29 @@
 package com.company;
 
+import java.util.Random;
+
 public class Test{
+    private String name;
+    private double salary;
+    private int id;
+    {
+        Random generator = new Random();
+        id = 1 + generator.nextInt(1_000_000);
+    }
+
+    public void raiseSalary(double byPercent){
+        double raise = this.salary*byPercent/100;
+        this.salary += raise;
+    }
+    public String getName(){
+        return name;
+    }
+    public Test(String name, double salary){
+        this.name = name;
+        this.salary = salary;
+    }
+    public Test(double salary){
+        this("",salary);
+    }
 
 }
