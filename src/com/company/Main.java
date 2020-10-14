ca90ea2876c4164a197e5336f36ab2c5d9ae11ce
hlindenau <hubert.lindenau@protonmail.com>
2020-10-14 15:06:45 +0200

    Intellij github user change test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,17 +9,8 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<String> friends = new ArrayList<>();
-        friends.add("Piotr");
-        Test t = new Test("Me",0);
-        Test anonim = new Test(100);
-        t.raiseSalary(10);
 
-        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance();
-        NumberFormat percentFormatter = NumberFormat.getPercentInstance();
-        double x = 0.1;
-        System.out.println(currencyFormatter.format(x));
-        System.out.println(percentFormatter.format(x));
-
-    }
+        //Testing github user change
+        
+        }
 }
